fbcce5466815b493a23f0a3ec9dc9be463823e5a
Christoph Jerolimov <jerolimov+git@gmail.com>
2024-10-29 12:40:28 +0100

    Add minimal helm examples

---
+++ scripts/get-cluster-route-suffix.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 1. requires `oc` and `yq`
+# 2. `oc` cli must connected to to an OpenShift cluster with
+#    read access to `ConfigMap` `cluster-config-v1`
+#    in `kube-system` namespace.
+
+set -euo pipefail
+
+# read install config
+configMap=$(oc get configmap -n kube-system cluster-config-v1 -o yaml)
+
+# extract from install-config
+installConfig=$(echo "$configMap" | yq '.data.install-config')
+
+# extract url
+clusterName=$(echo "$installConfig" | yq '.metadata.name')
+clusterBaseDomain=$(echo "$installConfig" | yq '.baseDomain')
+
+clusterRouteSuffix="apps.$clusterName.$clusterBaseDomain"
+
+echo "$clusterRouteSuffix"
