d45000e2996a9b8c3affb8ac3f1011fb963f2574
Christoph Jerolimov <jerolimov+github@redhat.com>
2024-11-21 12:59:22 +0100

    simple route suffix script, that now works also on rosa

--- scripts/get-cluster-route-suffix.sh
+++ scripts/get-cluster-route-suffix.sh
@@ -7,16 +7,18 @@
 
 set -euo pipefail
 
-# read install config
-configMap=$(oc get configmap -n kube-system cluster-config-v1 -o yaml)
+# # read install config
+# configMap=$(oc get configmap -n kube-system cluster-config-v1 -o yaml)
 
-# extract from install-config
-installConfig=$(echo "$configMap" | yq '.data.install-config')
+# # extract from install-config
+# installConfig=$(echo "$configMap" | yq '.data.install-config')
 
-# extract url
-clusterName=$(echo "$installConfig" | yq '.metadata.name')
-clusterBaseDomain=$(echo "$installConfig" | yq '.baseDomain')
+# # extract url
+# clusterName=$(echo "$installConfig" | yq '.metadata.name')
+# clusterBaseDomain=$(echo "$installConfig" | yq '.baseDomain')
 
-clusterRouteSuffix="apps.$clusterName.$clusterBaseDomain"
+# clusterRouteSuffix="apps.$clusterName.$clusterBaseDomain"
 
-echo "$clusterRouteSuffix"
+# echo "$clusterRouteSuffix"
+
+oc -n openshift-console get routes.route.openshift.io/console -o jsonpath='{.spec.host}' | sed 's/.*\.apps\./apps./g'
